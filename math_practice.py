87ed48c6d6f91173527205b8834fa31876ebb007
breaddon <brendonnguyen24@gmail.com>
2026-09-07 13:23:23 +1000

    Add functions to get values and reverse numbers

---
+++ math_practice.py
@@ -0,0 +1,16 @@
+def get_value(values: list, number: int):
+    output = []
+
+    for value in values:
+        for digit in value:
+            if digit == str(number):
+                output.append(value)
+                break
+    return output
+
+print(get_value(values=['123', '8273', '1818', '1725', '333'], number=3))
+
+
+def reversal(num):
+    return int(str(num)[::-1])
+print(reversal(123456))
