6093f26871f0284dc81701f1da36dbd039aa2966
breaddon <brendonnguyen24@gmail.com>
2026-09-07 13:26:04 +1000

    Update math_practice.py

--- math_practice.py
+++ math_practice.py
@@ -10,7 +10,16 @@ def get_value(values: list, number: int):
 
 print(get_value(values=['123', '8273', '1818', '1725', '333'], number=3))
 
-
+ 
 def reversal(num):
     return int(str(num)[::-1])
 print(reversal(123456))
+
+
+def sum_until(n):
+    output = 0
+    for i in range(1, n + 1):
+        output += i
+    return output
+
+print(sum_until(10))
